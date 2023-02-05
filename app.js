087079ed57422818a987878488204d48b639842b
Tushar Kumar <83531175+Tushar-byte-ctreate@users.noreply.github.com>
2023-02-05 17:19:26 +0530

    add secrte in

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ dotenv.config();
 
 //   app.use(cookieParser('keyboard cat'));
 app.use(session({
-    secret:process.env.SES_SECRET,
+    secret:process.env.EMAIL_USER,
     secure:"true",
     resave:true,
     saveUninitialized:true,
